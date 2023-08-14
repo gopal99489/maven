f0f9cc63e6d27088e6e7e8cf946a7bf92a302bdb
gopal99489 <127981548+gopal99489@users.noreply.github.com>
2023-08-15 00:57:19 +0530

    Update SimpleSpringbootAppApplication.java

--- src/main/java/com/example/simplespringbootapp/SimpleSpringbootAppApplication.java
+++ src/main/java/com/example/simplespringbootapp/SimpleSpringbootAppApplication.java
@@ -2,15 +2,17 @@ package com.example.simplespringbootapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping; // Add this import
+import org.springframework.web.bind.annotation.RestController; // Add this import
 
 @SpringBootApplication
 public class SimpleSpringbootAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimpleSpringbootAppApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringbootAppApplication.class, args);
+    }
 }
+
 @RestController
 class TaskController {
 
